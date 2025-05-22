b5b448129697ff72079ddae836983f93776370d6
unknown <andreaquiroga010@gmail.com>
2025-05-22 17:13:15 -0500

    hola

---
+++ src/main/java/com/mycompany/primer/Primer.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.primer;
+
+/**
+ *
+ * @author DELL
+ */
+public class Primer {
+
+    public static void main(String[] args) {
+        System.out.println("Hello mundo!");
+    }
+}
